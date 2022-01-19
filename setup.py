b0915664c24b64ac7cf69d51bf40622af34cce73
AdrianvEik <85258266+AdrianvEik@users.noreply.github.com>
2022-01-19 22:49:24 +0100

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
-from setuptools import setup, find_namespace_packages
+from setuptools import setup, find_packages
 
 setup(
     name="AdrianPack",
     author="Adrian v Eik",
     version="0.0.1",
+    license="MIT",
+    packages=find_packages(),
     install_requires=["numpy", "pandas", "matplotlib"],
     tests_require=["pytest"]
 )
