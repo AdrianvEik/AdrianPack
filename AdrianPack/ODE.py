076db2564fd6e46c601595efcb7c68112f138b24
AdrianvEik <85258266+AdrianvEik@users.noreply.github.com>
2022-05-10 20:22:47 +0200

    VERSION 0.0.2 RELEASE
    Added runga kutta approximation  methods for ODEs to ODE.py.

--- AdrianPack/ODE.py
+++ AdrianPack/ODE.py
@@ -1,121 +1,202 @@
-
 import numpy as np
 from csvread import csvread
 from typing import Callable
 
 test_input = csvread.test_inp
 
-class Base():
-    def __init__(self, *args, **kwargs):
-        """"
-        Base function with definitions of self and shared functions
-        """
 
+def euler(fx: Callable, lower: float, upper: float, dt: float,
+          x0: float = 0) -> tuple:
+    """
+    Evaluate differential equations using eulers method
 
-        # TODO: UMPDATE ERROR THROWS
-        if "fx" in kwargs:
-            self.fx: Callable = kwargs["fx"]
-        elif len(args) > 1:
-            self.fx: Callable = args[0]
-        else:
-            raise AttributeError("Function requires fx")
+    USAGE:
+        # define the derivative of a function fx that takes an "x" and a "t"
+        # input
 
-        if "dt" in kwargs:
-            self.dt: float = kwargs["dt"]
-        elif len(args) > 2:
-            self.dt: float = args[1]
-        else:
-            self.dt = 0.01
+        def fx(x, t):
+            return np.sin(x) * -t**3
 
-    def import_cuda(self):
-        pass
+        # define lower and upper boundaries, a timestep dt and *optional*
+        # a starting position x0
+        lower = 5
+        upper = 10
+        dt = 0.0001
+        x0 = 3
 
-
-class Solve_ODE(Base):
-    """"
+        # call the function and assign the returns to tpoints and xpoints
+        tpoints, xpoint = euler(fx=fx, lower=lower, upper=upper, dt=dt, x0=x0)
 
     :param: fx
-        ObjectType -> function
-
-        Function fx which is the first derivative of function
-
-    :param: dt
-        ObjectType -> float
-
-        Stepsize used when solving the ODE
+        ObjectType -> Function
+        Function that takes the input fx(x, t) and returns a value y.
 
     :param: lower
         ObjectType -> float
+        Lower boundary for the solution of the differential equation.
 
     :param: upper
         ObjectType -> float
+        Upper boundary for the solution of the differential equation.
 
-    :param: mode
-        ObjectType -> str
+    :param: dt
+        ObjectType -> float
+        Time step for the function, this defines the length of the returned
+        array, lowering this value will impact the time it takes to return
+        the values.
+    :param: x0
+        ObjectType -> float
+        Starting point of the graph x0.
 
-        Method to calculate the result either "Euler" or "GK".
+    :returns:
+        ObjectType -> Union[np.ndarray, np.ndarray]
+        returns tpoints and xpoints of the original function Fx.
+
     """
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
 
-        if "mode" in kwargs:
-            self.mode: str = kwargs["mode"]
-        else:
-            self.mode = "single"
+    N = round(np.abs((lower - upper)) / dt)
 
-        if "lower" in kwargs:
-            self.lower: float = kwargs["lower"]
-        else:
-            raise AttributeError("Function requires lower")
+    tpoints: np.ndarray = np.linspace(lower, upper, N)
+    xpoints: np.ndarray = np.zeros(N)
 
-        if "upper" in kwargs:
-            self.upper: float = kwargs["upper"]
-        else:
-            raise AttributeError("Function requires upper")
+    xpoints[0] = x0
+    for i in range(len(tpoints) - 1):
+        xpoints[i + 1] = xpoints[i] + dt * fx(xpoints[i], tpoints[i])
+    return tpoints, xpoints
 
-        if "dt" in kwargs:
-            self.dt: float = kwargs["dt"]
-        else:
-            raise AttributeError("Function requires dt")
 
-        if "x0" in kwargs:
-            self.x0: float = kwargs["x0"]
-        else:
-            self.x0 = 0
+def runga_kutta_4(fx: Callable, lower: float, upper: float, dt: float,
+                x0: float = 0) -> tuple:
+    """
+        Evaluate differential equations using a 4th order Runga-Kutta
+         approximation
 
-        self.N = round(np.abs((self.lower - self.upper)) / self.dt)
+        USAGE:
+            # define the derivative of a function fx that takes an "x" and a "t"
+            # input
 
-        self.tpoints = np.linspace(self.lower, self.upper, self.N)
-        self.xpoints = np.zeros(self.N)
+            def fx(x, t):
+                return np.sin(x) * -t**3
 
-        self.xpoints[0] = self.x0
+            # define lower and upper boundaries, a timestep dt and *optional*
+            # a starting position x0
+            lower = 5
+            upper = 10
+            dt = 0.0001
+            x0 = 3
 
-    def __call__(self) -> tuple:
+            # call the function and assign the returns to tpoints and xpoints
+            tpoints, xpoint = euler(fx=fx, lower=lower, upper=upper, dt=dt, x0=x0)
+
+        :param: fx
+            ObjectType -> Function
+            Function that takes the input fx(x, t) and returns a value y.
+
+        :param: lower
+            ObjectType -> float
+            Lower boundary for the solution of the differential equation.
+
+        :param: upper
+            ObjectType -> float
+            Upper boundary for the solution of the differential equation.
+
+        :param: dt
+            ObjectType -> float
+            Time step for the function, this defines the length of the returned
+            array, lowering this value will impact the time it takes to return
+            the values.
+        :param: x0
+            ObjectType -> float
+            Starting point of the graph x0.
+
+        :returns:
+            ObjectType -> Union[np.ndarray, np.ndarray]
+            returns tpoints and xpoints of the original function Fx.
+
         """
-        return calculated values in format tuple(t, x)
+
+    N = round(np.abs((lower - upper)) / dt)
+
+    tpoints: np.ndarray = np.linspace(lower, upper, N)
+    xpoints: np.ndarray = np.zeros(N)
+
+    xpoints[0] = x0
+    for i in range(len(tpoints) - 1):
+        kutta_constants = np.zeros(4)
+        kutta_constants[0] = dt * fx(xpoints[i], tpoints[i])
+        kutta_constants[1] = dt * fx(xpoints[i] + 1/2 * kutta_constants[0],
+                                     tpoints[i] + 1/2 * dt)
+        kutta_constants[2] = dt * fx(xpoints[i] + 1/2 * kutta_constants[1],
+                                     tpoints[i] + 1/2 * dt)
+        kutta_constants[3] = dt * fx(xpoints[i] + kutta_constants[2],
+                                         tpoints[i] + dt)
+
+        xpoints[i + 1] = xpoints[i] + 1/6 * (sum(2 * kutta_constants[1:-1]) +
+                                             kutta_constants[0] + kutta_constants[-1])
+    return tpoints, xpoints
+
+
+def runga_kutta_2(fx: Callable, lower: float, upper: float, dt: float,
+                  x0: float = 0) -> tuple:
+    """
+        Evaluate differential equations using a 2nd order Runga-Kutta
+         approximation
+
+        USAGE:
+            # define the derivative of a function fx that takes an "x" and a "t"
+            # input
+
+            def fx(x, t):
+                return np.sin(x) * -t**3
+
+            # define lower and upper boundaries, a timestep dt and *optional*
+            # a starting position x0
+            lower = 5
+            upper = 10
+            dt = 0.0001
+            x0 = 3
+
+            # call the function and assign the returns to tpoints and xpoints
+            tpoints, xpoint = euler(fx=fx, lower=lower, upper=upper, dt=dt, x0=x0)
+
+        :param: fx
+            ObjectType -> Function
+            Function that takes the input fx(x, t) and returns a value y.
+
+        :param: lower
+            ObjectType -> float
+            Lower boundary for the solution of the differential equation.
+
+        :param: upper
+            ObjectType -> float
+            Upper boundary for the solution of the differential equation.
+
+        :param: dt
+            ObjectType -> float
+            Time step for the function, this defines the length of the returned
+            array, lowering this value will impact the time it takes to return
+            the values.
+        :param: x0
+            ObjectType -> float
+            Starting point of the graph x0.
+
+        :returns:
+            ObjectType -> Union[np.ndarray, np.ndarray]
+            returns tpoints and xpoints of the original function Fx.
+
         """
-        getattr(self, self.mode.lower())()
-        return self.tpoints, self.xpoints
 
-    def euler(self):
-        """
+    N = round(np.abs((lower - upper)) / dt)
 
-        """
-        for i in range(len(self.tpoints) - 1):
-            self.xpoints[i + 1] = self.xpoints[i] +\
-                                  self.dt * self.fx(self.xpoints[i], self.tpoints[i])
+    tpoints: np.ndarray = np.linspace(lower, upper, N)
+    xpoints: np.ndarray = np.zeros(N)
 
-    def gk(self):
-        pass
+    xpoints[0] = x0
+    for i in range(len(tpoints) - 1):
+        kutta_constants = np.zeros(2)
+        kutta_constants[0] = dt * fx(xpoints[i], tpoints[i])
+        kutta_constants[1] = dt * fx(xpoints[i] + 1/2 * kutta_constants[0],
+                                     tpoints[i] + 1/2 * dt)
+        xpoints[i + 1] = xpoints[i] + kutta_constants[1]
 
-
-if __name__ == "__main__":
-    from Aplot import Default
-    def f(x, t):
-        return -x**3 + np.sin(t)
-
-    data = Euler(fx=f, lower=0, upper=10, dt=0.001, x0=10)()
-    print(data)
-    Default(data[0], data[1], degree=1)()
-
-
+    return tpoints, xpoints
