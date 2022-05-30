1e027778d3197c298e82406f1b39fd3b15559bae
AdrianvEik <85258266+AdrianvEik@users.noreply.github.com>
2022-05-30 17:24:50 +0200

    Add the option "show_fit" to .fit() method parameters, this will now return the fit coefficients and errors in a dictionary in the order specified by either fx or from n^ith polynomial where as n^i, n^i-1, n^i-2 ... n^0.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="AdrianPack",
     author="Adrian v Eik",
-    version="0.0.2",
+    version="0.0.2.1",
     license="MIT",
     packages=find_packages(),
     install_requires=["numpy", "pandas", "matplotlib", "scipy"],
